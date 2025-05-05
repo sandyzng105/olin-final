16295b1e2b352a4fe5764a964ea40d12e3e2039e
jiayi-shao <161370194+jiayi-shao@users.noreply.github.com>
2025-05-05 11:29:40 -0400

    Update create_db.py
    added dummy data; 
    added create table deliveries

--- create_db.py
+++ create_db.py
@@ -18,6 +18,7 @@ def create_database():
     DROP TABLE IF EXISTS Users;
     DROP TABLE IF EXISTS Products;
     DROP TABLE IF EXISTS Categories;
+    DROP TABLE IF EXISTS Deliveries;
 
     CREATE TABLE Categories (
         category_id INTEGER PRIMARY KEY,
@@ -83,6 +84,14 @@ def create_database():
         FOREIGN KEY (user_id) REFERENCES Users(user_id)
     );
 
+    CREATE TABLE Deliveries (
+        delivery_id INTEGER PRIMARY KEY,
+        order_id INTEGER,
+        address TEXT,
+        ts TEXT NOT NULL,
+        FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+    );
+
     CREATE TABLE Images (
         image_id INTEGER PRIMARY KEY,
         product_id INTEGER,
@@ -114,6 +123,49 @@ def create_database():
     ]
     c.executemany(
     "INSERT INTO Users (first_name, last_name, email, phone_number, shipping_address, billing_address, password) VALUES (?, ?, ?, ?, ?, ?, ?)", users)
+
+    #########################added more data
+   orders = [( 1, "May 1, 2025", "success"),
+    ( 1, "May 1, 2025", "failed"),
+    ( 2, "Apr 30, 2025", "success"),
+    ( 2, "May 2, 2025", "pending"),
+    ( 2, "May 3, 2025", "success"),
+    ( 2, "May 4, 2025", "success"),
+    ]
+    c.executemany(
+    "INSERT INTO Orders (user_id, order_date, status) VALUES (?, ?, ?)", orders)
+    
+
+    shopping_cart_items =[  (1, 1, 2, 20.0),
+                            (2, 2, 5, 33.0),
+                            (3, 3, 1, 11.5),
+                            (4, 3, 2, 23.0),
+                            (5, 4, 1, 11.5),
+                            (6, 4, 1, 11.5),
+    ]
+    c.executemany(
+    "INSERT INTO Shopping_Carts (order_id, product_id, quantity, subtotal) VALUES (?, ?, ?, ?)", shopping_cart_items)
+
+    user_reviews = [    (1, 1, 1, 4, "best product ever", "Apirl 10, 2025"),
+                        (2, 2, 3, 3, "just so so", "Apirl 12, 2025"),
+                        (3, 2, 5, 5, "perfect", "May 1, 2025"),
+                        (3, 2, 6, 4, "Bought another one", "May 1, 2025")
+    ]
+
+    c.executemany(
+    "INSERT INTO Reviews (product_id, user_id, order_id, rating, comment, ts) VALUES (?, ?, ?, ?, ?, ?)", user_reviews)
+
+
+    deliveries = [  ("Amphithreater Parkway 4900, CA 94043", "May 01, 2025", 1),
+                    ("106 Central St, Wellesley, MA 02481", "April 12, 2025", 3),
+                    ("106 Central St, Wellesley, MA 02481", "May 4, 2025", 5),
+                    ("106 Central St, Wellesley, MA 02481", "May , 2025", 6)
+    ]
+
+    c.executemany(
+    "INSERT INTO Deliveries (address, ts, order_id) VALUES (?, ?, ?)", deliveries)
+
+    #########################
     
     payment_methods = [
         (1, sha1_hash("4111111111111111"), "12/26", sha1_hash("123")),
